86b40b8c251dd76156a8728e5dc2dd4eca7b7d2c
m <example@example.com>
2024-03-20 21:03:48 -0500

    added notes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
 import WouldYouRather from './components/WouldYouRather.vue'
 
-import { ref } from 'vue'
+import { ref } from 'vue' //declare reactive state
 
+//declare reactive data for question, answer and userSelection
 const wyrQuestion = ref('Would you rather be able to read minds or see one day into the future?')
 const wyrAnswer1 = ref('Read Minds')
 const wyrAnswer2 = ref('See One Day into Future')
 
-// this will store the user's answer once they made a choice
-
+// this will store the user's answer once they made a choice (also reactive data)
 const userSelection = ref('')
 
 function updateUserSelection(userChoice){
@@ -25,6 +25,9 @@ function updateUserSelection(userChoice){
 
     <h1>Would You Rather?</h1>
 
+    <!--this information binds the reactive data in this parent with the reactive data in the child.
+    Doesn't have to be reactive data, could be regular string, but in this case it is reactive. Has to be a string, though
+    since that is what child is expecting based on Props info entered-->
     <WouldYouRather
         v-bind:question="wyrQuestion"
         v-bind:answer1="wyrAnswer1"
--- src/components/WouldYouRather.vue
+++ src/components/WouldYouRather.vue
@@ -1,17 +1,21 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch } from 'vue' //declare reactive state
 
+//props are what is shared by parent with child
 defineProps({
   question: String,
   answer1: String,
   answer2: String,
 })
 
+
 const emit = defineEmits([
     'answer-selected'
 ])
+//emit is what is sent to parent by child
 
 const choice = ref('')
+//leave choice constant open to receive choice made by user
 
 // function choiceMade() {
 //   //emit even to tell parent that user has made choice.
@@ -20,10 +24,13 @@ const choice = ref('')
 // }
 // this takes care of returning all the data, functions, computed properties, so return statement not needed.
 
+//watch for the choice made and emit the information to parent on event
 watch(choice, () => {
   emit('answer-selected', choice.value)
     })
 </script>
+
+
 
 <template>  <!--this is the HTML that VUE can use. Similar to <div id="app"> seen previously--> <!--'v-on:' is same as @click -->
 
@@ -31,8 +38,10 @@ watch(choice, () => {
     <h2>Make your choice!</h2>
 
     <p>{{ question }}</p>
+    <!-- 'question' is a prop provided by parent (app.vue) -->
 
       <div>
+        <!-- this value attribute is the HTML value, for what data an input stores -->
         <input v-model="choice" v-bind:value="answer1" type="radio" id="answer-1">
         <label for="answer-1">{{ answer1 }}</label>
 
